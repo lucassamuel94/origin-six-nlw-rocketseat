2e79d197b44cd27b19ae64a4fe64026bf941da9e
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-01-30 12:47:48 -0300

    create component ContainerSection

---
+++ src/components/ui/container-section.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/lib/utils'
+
+type ContainerSectionProps = {
+  children: React.ReactNode,
+  id?: string,
+  className?: string
+}
+
+export default function ContainerSection({
+  children,
+  id,
+  className
+}: ContainerSectionProps) {
+  return (
+    <section id={id} className={cn('container py-20 lg:py-40', className)}>
+      {children}
+    </section>
+  )
+}
