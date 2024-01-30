918c8077bc3345bc1e12d5cdda9feb04b0085645
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-01-30 19:20:05 -0300

    added component Sobre and Divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Divider from '@/components/ui/divider'
 import Hero from './components/hero'
+import Sobre from './components/sobre'
 
 export default function HomeApp() {
   return (
     <main>
       <Hero />
+      <Sobre />
+      <Divider className='rotate-180' />
     </main>
   )
 }
