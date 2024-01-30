d5210197eb67070214fd9cbd181307a255b6893c
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-01-30 12:47:30 -0300

    style update default styling and variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,26 +1,24 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-darken/90 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-neutral-900 text-neutral-50 hover:bg-neutral-900/90',
-        destructive: 'bg-red-500 text-neutral-50 hover:bg-red-500/90',
+        default: 'bg-brand-primary text-white hover:bg-brand-primary/90',
+        // destructive: 'bg-red-500 text-neutral-50 hover:bg-red-500/90',
         outline:
-          'border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900',
-        secondary: 'bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80',
-        ghost: 'hover:bg-neutral-100 hover:text-neutral-900',
-        link: 'text-neutral-900 underline-offset-4 hover:underline'
+          'border border-brand-secondary bg-white hover:bg-brand-secondary hover:text-brand-darken',
+        // secondary: 'bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80',
+        ghost: 'hover:bg-brand-secondary/30 hover:text-brand-primary',
+        link: 'text-brand-primary underline-offset-4 hover:underline'
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        default: ' px-8 py-4',
         icon: 'h-10 w-10'
       }
     },
